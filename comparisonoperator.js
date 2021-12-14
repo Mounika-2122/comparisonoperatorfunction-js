8a03d9fd5cee36db118bfee39fc35c05aeff1eb5
Mounika <bojjamounika8@gmail.com>
2021-12-15 00:17:29 +0530

    comparisonoperator

---
+++ comparisonoperator.js
@@ -0,0 +1,55 @@
+
+if (8 == "8") {
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+
+if (8 === "8") {
+   console.log(true);
+}else {
+    console.log(false);
+}
+
+
+if (8 != "8") {
+    console.log(true);
+}else {
+        console.log(false);
+}
+
+    
+if (8 !== "8") {
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+if (8 < "8") {
+     console.log(true);
+ }else {
+    console.log(false);
+}
+
+
+if (8 <= "8") {
+     console.log(true);
+}else {
+    console.log(false);
+ } 
+            
+ 
+if (8 > "8") {
+    console.log(true);
+}else {
+    console.log(false);
+} 
+
+
+if (8 >= "8") {
+    console.log(true);
+}else {
+    console.log(false);
+}          
+                
